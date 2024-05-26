8ca7289a5a93e9654a739bf02ed7b3f30ac876c3
MrWebXpert <mrwebxpert@gmail.com>
2024-05-26 13:55:09 +0500

    adding name

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,27 +5,27 @@ import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 const Hero = () => {
   return (
-    <section className="relative w-full h-screen mx-auto  ">
+    <section className="relative w-full h-screen mx-auto ">
       <div
         className={`${styles.paddingX} absolute mt-5 inset-0 top-[100px] mx-auto flex flex-row item-start gap-5 max-w-7xl`}
       >
-        <div className="flex flex-col justify-center items-center -mt-72">
+        <div className="flex flex-col items-center justify-center -mt-72">
           <div className="w-5 h-5 rounded-full bg-[#915eff] " />
-          <div className="w-1 sm:h-80 h-40 bg-gradient-to-b from-violet-500" />
+          <div className="w-1 h-40 sm:h-80 bg-gradient-to-b from-violet-500" />
         </div>
         <div>
           <h1 className={`${styles.heroHeadText}`}>
-            Hi , I'm <span className="text-[#915eff]"> Danyal Khan</span>
+            Hi , It's Me. <span className="text-[#915eff] font-semibold">MrHammadSandhu.</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            i devolop 3D visuals, user <br className="sm:block hidden" />{" "}
-            interface and web application
+            And I'm Full Stack Developer. <br className="hidden sm:block" />{" "}
+            and Designer
           </p>
         </div>
       </div>
       <ComputersCanvas />
 
-      <div className="absolute xs:bottom-1 bottom-1 w-full flex justify-center items-center">
+      <div className="absolute flex items-center justify-center w-full xs:bottom-1 bottom-1">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
@@ -35,7 +35,7 @@ const Hero = () => {
                 repeat: Infinity,
                 repeatType: "loop",
               }}
-              className="w-3 h-3 rounded-full bg-secondary mb-1"
+              className="w-3 h-3 mb-1 rounded-full bg-secondary"
             />
           </div>
         </a>
